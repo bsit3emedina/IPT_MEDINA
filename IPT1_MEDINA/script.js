509c2b23faca567a11055e1dee2f75fd40fad687
bsit3emedina <202202640@cityofmalabonuniversity.edu.ph>
2024-11-26 18:05:34 +0800

    Add files via upload
    PORTFOLIO

---
+++ IPT1_MEDINA/script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll for Navigation Links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').slice(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
